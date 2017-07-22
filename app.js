456449c43d82d2bdc126b314c1000ca2870558bf
Faith <fmugure@intracitygeeks.org>
2017-07-22 16:57:28 +0000

    my first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var todos = ["item 1", "item 2", "item 3"]
+var todosToo = ["item 1", "item 2", "item 3"]
+
+var faithFunc = function (something){
+    todosToo.push(something);
+    
+    console.log(todosToo);
+}
+
+
+
+
